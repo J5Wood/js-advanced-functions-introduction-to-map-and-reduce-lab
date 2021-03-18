7ab99f3c36a373c1d682115c29a09bedf199e77e
Jeremy Wood <js.wood206@gmail.com>
2021-03-17 20:52:50 -0700

    commit tree e0f7c48639411d0961d1b3ab8367ffa1c8f0cc14

--- index.js
+++ index.js
@@ -1,1 +1,57 @@
-// Your code here
+function mapToNegativize(arr) {
+    let negativeArray = [];
+    for ( let i = 0; i < arr.length; i++) {
+        negativeArray.push(arr[i] * -1);
+    }
+    return negativeArray;
+}
+
+function mapToNoChange(arr) {
+    let newArray = [];
+    return newArray.concat(arr);
+}
+
+function mapToDouble(arr) {
+    let doubleArray = [];
+    for ( let i = 0; i < arr.length; i++) {
+        doubleArray.push(arr[i] * 2);
+    }
+    return doubleArray;
+}
+
+function mapToSquare(arr) {
+    let squareArray = [];
+    for (let i = 0; i < arr.length; i++) {
+        squareArray.push(arr[i] ** 2);
+    }
+    return squareArray;
+}
+
+
+function reduceToTotal(arr, initialValue = 0) {
+    let reducedValue = initialValue;
+    for ( let i = 0; i < arr.length; i++ ) {
+        reducedValue += arr[i];
+    }
+    return reducedValue;
+}
+
+function reduceToAllTrue(arr) {
+    let truthCheck = true;
+    for ( let i = 0; i < arr.length; i++ ) {
+        if (arr[i] === false) {
+            truthCheck = false;
+        }
+    }
+    return truthCheck;
+}
+
+function reduceToAnyTrue(arr) {
+    let truthCheck = false;
+    for ( let i = 0; i < arr.length; i++) {
+        if (!!arr[i]) {
+            truthCheck = true;
+        }
+    }
+    return truthCheck;
+}
